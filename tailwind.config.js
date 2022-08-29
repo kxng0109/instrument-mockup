76141e7dd07818ab4f5f155b877278f7b4133db2
kxng0109 <iluhjosh13@gmail.com>
2022-08-30 00:41:59 +0100

    Done with the html. Now to style it.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'custom-blue' : '#6c83ff',
+        'custom-pink' : '#ffa98e',
+      },
+    },
   },
   plugins: [],
 }
