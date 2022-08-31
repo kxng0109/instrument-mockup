8774ad7e739f260dd92fe0645bda4b38cc036278
kxng0109 <iluhjosh13@gmail.com>
2022-08-31 17:11:11 +0100

    Did a little bit of things

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         'custom-blue' : '#6c83ff',
         'custom-pink' : '#ffa98e',
       },
+      zIndex:{
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+      },
     },
   },
   plugins: [],
