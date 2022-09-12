676e23323a15c0c439adeb2d310c42d405a8ca63
kxng0109 <iluhjosh13@gmail.com>
2022-09-12 02:29:05 +0100

    Started working on some typescript.

--- public/index.js
+++ public/index.js
@@ -3,3 +3,22 @@ const container = document.querySelector('.container');
 const main = document.querySelector('#main');
 const menuBtn = document.querySelector('#menu-btn');
 const navBar = document.querySelector('#navbar');
+const playBtn = document.querySelector('#playBtn');
+const videoTemplate = document.querySelector('#video-template');
+const iframeVideo = document.querySelector('#iframe-video');
+const whitePlayBg = document.querySelector('#white-play-bg');
+const testing = document.querySelector('.vp-controls');
+menuBtn.onclick = () => {
+    let displayValue = getComputedStyle(navBar, undefined).getPropertyValue('display');
+    displayValue == 'none' ?
+        (navBar.style.display = 'flex',
+            menuBtn.setAttribute('name', 'close')) :
+        (navBar.style.display = 'none',
+            menuBtn.setAttribute('name', 'menu'));
+};
+playBtn.onclick = () => {
+    playBtn.classList.add('hidden');
+    videoTemplate.classList.add('hidden');
+    whitePlayBg.classList.add('hidden');
+    /*Video needs to automatically play*/
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
 const container = document.querySelector('.container') as HTMLDivElement;
 const main = document.querySelector('#main') as HTMLDivElement;
-const menuBtn = document.querySelector('#menu-btn')!;
+const menuBtn = document.querySelector('#menu-btn') as HTMLButtonElement;
 const navBar = document.querySelector('#navbar') as HTMLDivElement;
+const playBtn = document.querySelector('#playBtn') as HTMLButtonElement;
+const videoTemplate = document.querySelector('#video-template') as HTMLVideoElement;
+const iframeVideo = document.querySelector('#iframe-video') as HTMLIFrameElement;
+const whitePlayBg = document.querySelector('#white-play-bg') as HTMLDivElement;
+const testing = document.querySelector('.vp-controls') as HTMLDivElement;
+
+menuBtn.onclick = () =>{
+	let displayValue = getComputedStyle(navBar, undefined).getPropertyValue('display');
+
+displayValue == 'none' ? 
+	(
+		navBar.style.display = 'flex',
+		menuBtn.setAttribute('name', 'close')
+	) : 
+	(
+		navBar.style.display = 'none',
+		menuBtn.setAttribute('name', 'menu')
+	)
+}
+
+playBtn.onclick = () =>{
+	playBtn.classList.add('hidden');
+	videoTemplate.classList.add('hidden');
+	whitePlayBg.classList.add('hidden');
+	/*Video needs to automatically play*/
+}
