44f0872d867a5661f89243734f9c53182f91c50d
kxng0109 <iluhjosh13@gmail.com>
2022-09-13 14:19:30 +0100

    Added the changing company logos towards the bottom of the page.

--- public/index.js
+++ public/index.js
@@ -7,7 +7,18 @@ const playBtn = document.querySelector('#playBtn');
 const videoTemplate = document.querySelector('#video-template');
 const iframeVideo = document.querySelector('#iframe-video');
 const whitePlayBg = document.querySelector('#white-play-bg');
-const testing = document.querySelector('.vp-controls');
+const firstLogoDiv = document.querySelector('#first-logo');
+const firstDivLogos = firstLogoDiv.querySelectorAll('.sixth--section--logos');
+const secondLogoDiv = document.querySelector('#second-logo');
+const secondDivLogos = secondLogoDiv.querySelectorAll('.sixth--section--logos');
+const thirdLogoDiv = document.querySelector('#third-logo');
+const thirdDivLogos = thirdLogoDiv.querySelectorAll('.sixth--section--logos');
+let firstPreviousImage, secondPreviousImage, thirdPreviousImage;
+//Generate a random number based on the number of images in that div and returns a random element
+let randomElement = (theDivLogos) => {
+    let randomNumber = Math.floor(Math.random() * theDivLogos.length);
+    return theDivLogos[randomNumber];
+};
 menuBtn.onclick = () => {
     let displayValue = getComputedStyle(navBar, undefined).getPropertyValue('display');
     displayValue == 'none' ?
@@ -22,3 +33,37 @@ playBtn.onclick = () => {
     whitePlayBg.classList.add('hidden');
     /*Video needs to automatically play*/
 };
+const logoAnimation = (previousElement, currentElement, theLogoDiv) => {
+    console.log(previousElement);
+    switch (true) {
+        case previousElement == undefined || previousElement == null:
+            currentElement.classList.add('show');
+            break;
+        case currentElement == previousElement:
+            break;
+        default:
+            currentElement.classList.add('show');
+            previousElement.classList.remove('show');
+            break;
+    }
+    switch (theLogoDiv) {
+        case firstDivLogos:
+            return firstPreviousImage = currentElement;
+            break;
+        case secondDivLogos:
+            return secondPreviousImage = currentElement;
+            break;
+        case thirdDivLogos:
+            return thirdPreviousImage = currentElement;
+            break;
+    }
+};
+setInterval(() => {
+    logoAnimation(firstPreviousImage, randomElement(firstDivLogos), firstDivLogos);
+}, 2000);
+setInterval(() => {
+    logoAnimation(secondPreviousImage, randomElement(secondDivLogos), secondDivLogos);
+}, 2000);
+setInterval(() => {
+    logoAnimation(thirdPreviousImage, randomElement(thirdDivLogos), thirdDivLogos);
+}, 2000);
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,24 @@ const playBtn = document.querySelector('#playBtn') as HTMLButtonElement;
 const videoTemplate = document.querySelector('#video-template') as HTMLVideoElement;
 const iframeVideo = document.querySelector('#iframe-video') as HTMLIFrameElement;
 const whitePlayBg = document.querySelector('#white-play-bg') as HTMLDivElement;
-const testing = document.querySelector('.vp-controls') as HTMLDivElement;
+const firstLogoDiv = document.querySelector('#first-logo') as HTMLDivElement;
+const firstDivLogos = firstLogoDiv.querySelectorAll('.sixth--section--logos') as NodeListOf<HTMLImageElement>;
+const secondLogoDiv = document.querySelector('#second-logo') as HTMLDivElement;
+const secondDivLogos = secondLogoDiv.querySelectorAll('.sixth--section--logos') as NodeListOf<HTMLImageElement>;
+const thirdLogoDiv = document.querySelector('#third-logo') as HTMLDivElement;
+const thirdDivLogos = thirdLogoDiv.querySelectorAll('.sixth--section--logos') as NodeListOf<HTMLImageElement>;
+let firstPreviousImage:HTMLImageElement, secondPreviousImage:HTMLImageElement, thirdPreviousImage:HTMLImageElement;
+
+//Generate a random number based on the number of images in that div and returns a random element
+let randomElement = (theDivLogos:NodeListOf<HTMLImageElement>) =>{
+	let randomNumber = Math.floor(Math.random() * theDivLogos.length);
+	return theDivLogos[randomNumber]
+}
 
 menuBtn.onclick = () =>{
 	let displayValue = getComputedStyle(navBar, undefined).getPropertyValue('display');
 
-displayValue == 'none' ? 
+	displayValue == 'none' ? 
 	(
 		navBar.style.display = 'flex',
 		menuBtn.setAttribute('name', 'close')
@@ -28,3 +40,46 @@ playBtn.onclick = () =>{
 	whitePlayBg.classList.add('hidden');
 	/*Video needs to automatically play*/
 }
+
+const logoAnimation = (previousElement:HTMLImageElement, currentElement:HTMLImageElement, theLogoDiv:NodeListOf<HTMLImageElement>) =>{
+		console.log(previousElement)
+	switch(true){
+		case previousElement == undefined || previousElement == null:
+			currentElement.classList.add('show');
+		break;
+
+		case currentElement == previousElement:
+		break;
+
+		default:
+			currentElement.classList.add('show');
+			previousElement.classList.remove('show');
+		break;
+	}
+
+	switch(theLogoDiv){
+		case firstDivLogos:
+			return firstPreviousImage = currentElement;
+		break;
+
+		case secondDivLogos:
+			return secondPreviousImage = currentElement;
+		break;
+
+		case thirdDivLogos:
+			return thirdPreviousImage = currentElement;
+		break;
+	}
+}
+
+setInterval(() => {
+	logoAnimation(firstPreviousImage, randomElement(firstDivLogos), firstDivLogos);
+}, 2000);
+
+setInterval(() => {
+	logoAnimation(secondPreviousImage, randomElement(secondDivLogos), secondDivLogos);
+}, 2000);
+
+setInterval(() => {
+	logoAnimation(thirdPreviousImage, randomElement(thirdDivLogos), thirdDivLogos);
+}, 2000);
